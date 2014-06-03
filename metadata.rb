0405178ff592e6afc37cd1af7c6e82f9357f9354
Michael Kirk <michael.john.kirk@gmail.com>
2014-06-03 12:21:22 -0700

    initial go

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rails-geo'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rails-geo'
+long_description 'Installs/Configures rails-geo'
+version          '0.1.0'
+
